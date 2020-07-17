b54aa51d22e63765190518b87ea62c67470fb9b4
Héctor Reyes <h.reyes@idventiva.com>
2020-07-17 09:52:32 -0500

    Curso Terminado

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -24,7 +24,7 @@ function useCategoriesData() {
   return { categories, loading };
 }
 
-export const ListOfCategories = () => {
+const ListOfCategoriesComponent = () => {
   const { categories, loading } = useCategoriesData();
   const [showFixed, setShowFixed] = useState(false);
 
@@ -64,3 +64,5 @@ export const ListOfCategories = () => {
     </Fragment>
   );
 };
+
+export const ListOfCategories = React.memo(ListOfCategoriesComponent);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
+const path = require("path");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   output: {
@@ -8,6 +11,42 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+    }),
+    new WebpackPwaManifestPlugin({
+      name: "Petgram - Tu app de fotos de mascotas",
+      // shortname= 'Petgram',
+      description:
+        "Con Petgram puedes encontrar fotos de animales domésticos muy fácilmente",
+      background_color: "#fff",
+      theme_color: "#b1a",
+      icons: [
+        {
+          src: path.resolve("src/assets/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            "https://res.cloudinary.com | images.unsplash.com"
+          ),
+          handler: "CacheFirst",
+          options: {
+            cacheName: "images",
+          },
+        },
+        {
+          urlPattern: new RegExp(
+            "https://petgram-server-hector.vercel.app/graphql"
+          ),
+          handler: "CacheFirst",
+          options: {
+            cacheName: "api",
+          },
+        },
+      ],
     }),
   ],
   module: {
